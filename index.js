e19f0187c04eb3f071dbda360822d5baffe7be4b
jalak <jalakpatoliya777@gmail.com>
2020-07-16 19:31:31 +0530

    deployment changes

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 /* eslint-disable no-undef */
 require('dotenv').config();
-const app = require('./src/app');
 const http = require('http');
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+/**
+ * Importing app
+ */
+const app = require('./src/app');
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    })
+}
+
+app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'))
+})
 
 /**
  * connecting to db
